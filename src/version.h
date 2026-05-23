2c71fc8a905ec197c3ac95046869ef4b3ba5692f
Dirk Tostmann <tostmann@gmail.com>
2026-05-23 18:24:35 +0200

    release: build counter bump for v0.7.5
    Consolidates the build snapshots produced by build_release.sh into a
    single release-marker commit. Webflasher artifacts in webflasher/ are
    generated separately by scripts/build_release.sh and are not committed
    (they're pushed to install.busware.de via rsync).

--- src/version.h
+++ src/version.h
@@ -7,9 +7,9 @@
 #define FW_NAME           "SixBack"
 #define FW_VERSION_MAJOR  0
 #define FW_VERSION_MINOR  7
-#define FW_VERSION_BUILD  728
+#define FW_VERSION_BUILD  736
 
-#define FW_VERSION_STRING "0.7.728"
-#define FW_BUILD_DATE     "2026-05-23 17:48:11"
+#define FW_VERSION_STRING "0.7.736"
+#define FW_BUILD_DATE     "2026-05-23 18:22:34"
 
 #endif // BOSEFIX32_VERSION_H
